c96db465f2124cc085286f93f8c881f2289d82da
Rami Majdoub <37843591+Rami-Majdoub@users.noreply.github.com>
2021-09-07 01:19:54 +0100

    replace spaces with tabs

--- gitus.sh
+++ gitus.sh
@@ -4,67 +4,67 @@
 # Help                                                     #
 ############################################################
 Help(){
-   # Display Help
-   echo "gitus is git but simpler."
-   echo
-   echo "gitus ([-m "commit message"] -s)|(-r)"
-   echo "options:"
-   echo "h          Print this Help."
-   echo "m          Add a commit message, default is \"Update\"."
-   echo "s          Send your changes to the cloud."
-   echo "r          Receive changes from the cloud."
-   echo
+	# Display Help
+	echo "gitus is git but simpler."
+	echo
+	echo "gitus ([-m "commit message"] -s)|(-r)"
+	echo "options:"
+	echo "h          Print this Help."
+	echo "m          Add a commit message, default is \"Update\"."
+	echo "s          Send your changes to the cloud."
+	echo "r          Receive changes from the cloud."
+	echo
 }
 
 send(){
-  # search for .git repo
-  has_git_repo=$(ls -a | grep "^.git/" | wc -l)
+	# search for .git repo
+	has_git_repo=$(ls -a | grep "^.git/" | wc -l)
 
-  if [ $has_git_repo = 0 ]; then
-    repo_name="$(basename "$PWD")"
-    user_name="$(git config --global --get user.name)"
+	if [ $has_git_repo = 0 ]; then
+		repo_name="$(basename "$PWD")"
+		user_name="$(git config --global --get user.name)"
 
-    git init
-    git branch -M main
-    git remote add origin git@github.com:"$user_name"/"$repo_name".git
-    if [ -z "$msg" ]; then
-      msg="first commit"
-    fi
-  fi
-  if [ -z "$msg" ]; then
-    msg="Update"
-  fi
+		git init
+		git branch -M main
+		git remote add origin git@github.com:"$user_name"/"$repo_name".git
+		if [ -z "$msg" ]; then
+			msg="first commit"
+		fi
+	fi
+	if [ -z "$msg" ]; then
+		msg="Update"
+	fi
 
-  git add -A
-  git commit -m "$msg"
+	git add -A
+	git commit -m "$msg"
 
-  remote="$(git remote show)"
-  current_branch="$(git branch --show-current)"
-  git push -u "$remote" "$current_branch"
+	remote="$(git remote show)"
+	current_branch="$(git branch --show-current)"
+	git push -u "$remote" "$current_branch"
 }
 
 recieve(){
-  git pull
+	git pull
 }
 
 while getopts "hm:sr" flag;
 do
-    case "${flag}" in
-        h) Help
-           exit;;
+	case "${flag}" in
+		h) Help
+		exit;;
 
-        m) msg=${OPTARG}
-           ;;
+		m) msg=${OPTARG}
+		;;
 
-        s) echo Sending your changes to the cloud.
-           send
-           ;;
+		s) echo Sending your changes to the cloud.
+		send
+		;;
 
-        r) echo Receiving changes from the cloud.
-           recieve
-           ;;
+		r) echo Receiving changes from the cloud.
+		recieve
+		;;
 
-        \?) # Invalid option
-           exit;;
-    esac
+		\?) # Invalid option
+		exit;;
+	esac
 done
--- install.sh
+++ install.sh
@@ -4,32 +4,32 @@ chmod +x ./gitus.sh
 
 started_edit=false
 beginEdit(){
-  if [ $started_edit = false ]; then
-    echo "##### Begin of gitus edit #####" >> ~/.profile
-    started_edit=true
-  fi
+	if [ $started_edit = false ]; then
+		echo "##### Begin of gitus edit #####" >> ~/.profile
+		started_edit=true
+	fi
 }
 
 endEdit(){
-  if [ $started_edit = true ]; then
-    echo "##### End of gitus edit #####" >> ~/.profile
-  fi
+	if [ $started_edit = true ]; then
+		echo "##### End of gitus edit #####" >> ~/.profile
+	fi
 }
 
 # add gitus directory to PATH
 path_contains_dir=$(cat ~/.profile | grep $PWD | wc -l)
 if [ $path_contains_dir -eq 0 ]; then
-  echo "Adding this directory to PATH"
-  beginEdit
-  echo "export PATH=\"$PATH:$PWD\"" >> ~/.profile
+	echo "Adding this directory to PATH"
+	beginEdit
+	echo "export PATH=\"$PATH:$PWD\"" >> ~/.profile
 fi
 
 # gitus.sh > gitus
 alias_exists=$(cat ~/.profile | grep "alias gitus=gitus.sh" | wc -l)
 if [ $alias_exists -eq 0 ]; then
-  echo "Creating alias"
-  beginEdit
-  echo alias gitus=gitus.sh >> ~/.profile
+	echo "Creating alias"
+	beginEdit
+	echo alias gitus=gitus.sh >> ~/.profile
 fi
 
 endEdit
