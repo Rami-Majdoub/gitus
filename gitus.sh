2faac02856e3533b657b705e4df320f70802f2a7
rami-majdoub <rami.majdoub1@gmail.com>
2022-05-29 08:47:48 +0100

    add learning mode

--- gitus.sh
+++ gitus.sh
@@ -7,89 +7,117 @@ Help(){
 	# Display Help
 	echo "gitus is git but simpler."
 	echo
-	echo "gitus ([-m \"commit message\"] -s)|(-r)"
+	echo "gitus ([-m \"commit message\"] -s) | -r | -(h|l|u)"
 	echo "options:"
 	echo "h          Print this Help."
 	echo "m          Add a commit message, default is \"Update\"."
 	echo "s          Send your changes to the cloud."
 	echo "r          Receive changes from the cloud."
+	echo "l          Activate/deactivate learning mode."
+	echo "u          Update gitus."
 	echo
+}
+
+debug=$(git config --bool --default true gitus.debug)
+Run(){
+	if [[ $debug = true ]]; then
+		echo "[gitus] $ $@"
+	fi
+	"$@"
+}
+
+git_vars_not_set_msg_shown=false
+CheckVar(){
+	var=$1
+	message=$2
+	tmp=$(git config --global --get "$var")
+	if [ -z "$tmp" ]; then
+		if [ $git_vars_not_set_msg_shown = false ]; then
+			echo "[gitus] Oh no! you haven't set git global variables yet. Let's do that"
+
+			git_vars_not_set_msg_shown=true
+		fi
+
+		read -p "$message" tmp
+		Run git config --global "$var" "$tmp"
+	fi
 }
 
 CheckSSH(){
 	if [ ! -f ~/.ssh/id_rsa ] ; then
+		CheckVar "user.email" "[gitus] what is you'r email? "
+
 		email=$(git config --global --get "user.email")
-		if [ -z "$email" ]; then
-			read -p "[gitus] what is you're email? " email
-		fi
 
 		echo "[gitus] Generating SSH Key"
-		ssh-keygen -t rsa -b 4096 -C "$email" -f "~/.ssh/id_rsa"
-		eval "$(ssh-agent -s)"
-		ssh-add ~/.ssh/id_rsa
+		Run ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa
+		Run eval "$(ssh-agent -s)"
+		Run ssh-add ~/.ssh/id_rsa
 
-		echo "[gitus] Please add the SSH key to your account before proceeding. For more information"
+		echo
+		echo "[gitus] ⚠️  Please add the SSH key to your account before proceeding. For more information"
 		echo "[gitus] GitHub: https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account"
+		read -p "[gitus] press Enter to continue "
 	fi
 }
 
 Init(){
-	git_vars_not_set_msg_shown=false
-
-	CheckVar(){
-		var=$1
-		message=$2
-		tmp=$(git config --global --get "$var")
-		if [ -z "$tmp" ]; then
-			if [ $git_vars_not_set_msg_shown = false ]; then
-				echo "[gitus] Oh no! you haven't set git global variables yet. Let's do that"
-
-				git_vars_not_set_msg_shown=true
-			fi
-
-			read -p "$message" tmp
-			git config --global --add "$var" "$tmp"
-		fi
-	}
-
-	CheckVar "user.name" "[gitus] what is you're user name? "
-	CheckVar "user.email" "[gitus] what is you're email? "
+	CheckVar "user.name" "[gitus] what is you'r user name? "
+	CheckVar "user.email" "[gitus] what is you'r email? "
+	git config --global init.defaultBranch main # only 1 time !
 }
 
 Send(){
+	Init
+	CheckSSH
+
 	# search for .git repo
 	has_git_repo=$( ls -ap | egrep "^\.git/$" | wc -l )
-
 	if [ $has_git_repo = 0 ]; then
-		Init
-		CheckSSH
-		repo_name="$(basename "$PWD")"
-		user_name="$(git config --global --get user.name)"
+		Run git init
 
-		git init
-		git branch -M main
-		git remote add origin git@github.com:"$user_name"/"$repo_name".git
-		
 		if [ -z "$msg" ]; then
 			msg="first commit"
 		fi
 	fi
+
+	# the remote is not set for new repos and generated projet
+	if [ -z "$(git remote)" ]; then
+		repo_name="$(basename "$PWD")"
+		user_name="$(git config --global --get user.name)"
+
+		Run git remote add origin git@github.com:"$user_name"/"$repo_name".git
+		had_no_remote=true # to set default branch to main after commit
+	fi
+
 	if [ -z "$msg" ]; then
 		msg="Update"
 	fi
-	git add -A
-	git commit -m "$msg"
+	Run git add -A
+	Run git commit -m "\"$msg\"" # outer quotes needed for Run(), inner quotes for Run.echo
+	if [[ $had_no_remote = true ]]; then
+		Run git branch -M main # needed only one time after first commit
+	fi
 
 	remote="$(git remote show)"
 	current_branch="$(git branch --show-current)"
-	git push -u "$remote" "$current_branch"
+	Run git push -u "$remote" "$current_branch"
 }
 
+###
+# fixme: pull is not working
+# getopts ignores -r when it doesn't have value
+###
 Recieve(){
-	git pull
+	# echo "url: $clone_url"
+	if [ -z "$clone_url" ]; then
+		Run git pull # didn't made it to this line !
+	else
+		Run git clone "$clone_url"
+	fi
 }
 
-while getopts "hm:sr" flag;
+while getopts ":r:hm:slu" flag;
 do
 	case "${flag}" in
 		h) Help
@@ -103,7 +131,26 @@ do
 		;;
 
 		r) echo "[gitus] Receiving changes from the cloud."
+		clone_url=${OPTARG}
 		Recieve
+		;;
+
+		l)
+		d=$(git config --bool --default true gitus.debug)
+		if [[ "$d" = true ]]; then
+			echo "[gitus] Learning mode is not active"
+			git config --global "gitus.debug" false
+		else
+			echo "[gitus] Learning mode is active"
+			git config --global "gitus.debug" true
+		fi
+		;;
+
+		u)
+			echo "[gitus] Downloading updates"
+			gitus_folder=$(dirname -- "$0")
+			cd $gitus_folder
+			git pull
 		;;
 
 		\?) # Invalid option
